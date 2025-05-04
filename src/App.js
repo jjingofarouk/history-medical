8265919ed08a5d29da46e360ce5e4f3d99c2f2bb
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-05 00:38:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import {
   User, AlertTriangle, FileText, Building2,
   Users, Globe, Stethoscope, Search, ArrowLeft
