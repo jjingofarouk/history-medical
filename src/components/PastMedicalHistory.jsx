63120beb58131aa3913b815f0ad280d08d3bba70
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-05 04:01:59 +0300

    Update PastMedicalHistory.jsx

--- src/components/PastMedicalHistory.jsx
+++ src/components/PastMedicalHistory.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CustomSelect from './CustomSelect';
 import { chronicConditionsOptions } from './ChronicConditions';
 import { specificIllnessesOptions } from './SpecificIllnesses';
