68d36d514e2994eefd1d7539bfb43c1ea2c5a0f6
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-09 00:12:38 +0300

    Update InputGroup.js

---
+++ src/components/InputGroup.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../PersonalInfo.css'; // Adjust path if CSS is in a different directory
+
+const InputGroup = ({ children }) => <div className="input-group">{children}</div>;
+
+export default InputGroup;
