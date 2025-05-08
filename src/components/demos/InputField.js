c6df03287ecec1932ac933159d0147e83d730928
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-09 00:19:02 +0300

    Update InputField.js

---
+++ src/components/demos/InputField.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FieldWrapper from './FieldWrapper';
+import '../PersonalInfo.css';
+
+const InputField = ({ label, name, type = 'text', value, onChange, placeholder, clinicalInfo }) => (
+  <FieldWrapper label={label} clinicalInfo={clinicalInfo}>
+    <input
+      type={type}
+      className="input-text"
+      placeholder={placeholder}
+      value={value || ''}
+      onChange={onChange}
+    />
+  </FieldWrapper>
+);
+
+export default InputField;
