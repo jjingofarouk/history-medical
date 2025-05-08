f3619fa3954a3d4901d56e35c3c456def8ef1c31
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-09 00:18:38 +0300

    Update InfoIcon.js

---
+++ src/components/demos/InfoIcon.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import '../PersonalInfo.css';
+
+const InfoIcon = ({ clinicalInfo }) => {
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
+
+  const toggleTooltip = () => {
+    setTooltipVisible(!isTooltipVisible);
+  };
+
+  return (
+    <span className="info-icon-container">
+      <span className="info-icon" onClick={toggleTooltip}>
+        i
+      </span>
+      <div className={`tooltip ${isTooltipVisible ? '' : 'hidden'}`}>
+        {clinicalInfo}
+      </div>
+    </span>
+  );
+};
+
+export default InfoIcon;
